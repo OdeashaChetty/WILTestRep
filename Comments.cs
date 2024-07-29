fae05d85871a737993c8f02886204adfb2b6b13d
ST10095103 <ST10095103@vcconnect.edu.za>
2024-07-29 12:06:13 +0200

    Add project files.

---
+++ Comments.cs
@@ -0,0 +1,12 @@
+﻿namespace WILTestDesignSpace
+{
+    public class Comments
+    {
+        //hey Randy, this is just a nonesense class i created so we can comment anything new and what we wish to communicate,
+        //i felt like it may be easier to do it here than to go back and forth and message while we coding (we may forget)
+       // also i needed something to push
+         //   Let me know how you would like to divide the pages 
+         //Odeasha (29/07)
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WILTestDesignSpace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
