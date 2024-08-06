17874d814c827b4a3c49d073c03ba6d5968a7389
RadhyaC <ST10069610@vcconnect.edu.za>
2024-08-06 10:33:49 +0200

    Did shit for donate page

---
+++ Controllers/Donate.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WIL_Project.Controllers
+{
+    public class Donate : Controller
+    {
+        public IActionResult DonatePage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/Projects.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WIL_Project.Controllers
+{
+    public class Projects : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
