018cc5bb951189ef726f69dc99ae4d826bf8539b
ST10095103 <ST10095103@vcconnect.edu.za>
2024-09-02 17:53:10 +0200

    Added The request status pages for Org

---
+++ Controllers/Organisation.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WILTestDesignSpace.Controllers
+{
+    public class Organisation : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Approved()
+        {
+            return View();
+        }
+        public IActionResult Rejected()
+        {
+            return View();
+        }
+    }
+}
